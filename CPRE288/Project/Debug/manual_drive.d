24622ed0588259fbf0fc322c6b1a9844ba4a0b2d
Trana25 <trana25@iastate.edu>
2026-05-12 00:49:39 -0500

    file deletion

--- CPRE288/Project/Debug/manual_drive.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-manual_drive.obj: ../manual_drive.c
-
-../manual_drive.c:
-
--- CPRE288/Project/coords.c
+++
@@ -1,7 +0,0 @@
-#include "coords.h"
-
-volatile coords_t coords = {
-    .x = 0.0f,
-    .y = 0.0f,
-    .angle = 0
-};
